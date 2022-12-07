14fd31dab6525c725cfdbf20e16c14406d31e64e
Saeed Panahi <saeed.panahi@nova.local>
2022-12-07 11:50:13 +0330

    add login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-// import Jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 /* REGISTER USER */
@@ -22,3 +22,22 @@ export const Register = async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 };
+
+/* LOGIN USER */
+export const Login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: "User does not exist" });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({ token });
+    }
+    catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { Register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js"
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -37,6 +38,9 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), Register)
+
+/* ROUTES */
+app.use("/auth", authRoutes)
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
